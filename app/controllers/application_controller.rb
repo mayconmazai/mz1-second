3205c9a097df58b15cbe3a4b43a30d1c6edaee9d
Maycon Mazai <mayconmazai@gmail.com>
2022-06-10 10:39:30 -0300

    Inserido index no controle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  def index
+  end
   # Add this line to see the progress bar long enough
   # and remove it when it has the expected styles
   # before_action -> { sleep 3 }
