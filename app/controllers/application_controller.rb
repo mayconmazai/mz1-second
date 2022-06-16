f3afb3cd7362e87f59869094b008bcefe312ab28
Maycon Mazai <mayconmazai@gmail.com>
2022-06-16 15:24:05 -0300

    Security login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
 
-  def index
-  end
+  before_action :authenticate_user!, unless: :devise_controller?
   # Add this line to see the progress bar long enough
   # and remove it when it has the expected styles
   # before_action -> { sleep 3 }
+
+  private
+
+  def current_company
+    @current_company ||= current_user.company if user_signed_in?
+  end
+  helper_method :current_company
 end
